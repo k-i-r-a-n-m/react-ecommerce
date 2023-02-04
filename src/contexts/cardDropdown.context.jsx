dc1f17a7e6f1326fc7dd05b54474a6cddb47df72
kiran <mkiranmsg@gmail.com>
2023-02-04 15:48:27 +0530

    checkout page styled and <checkoutItem> added

--- src/contexts/cardDropdown.context.jsx
+++ src/contexts/cardDropdown.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState,useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
   // find if cartItems contains productToAdd
@@ -20,16 +20,25 @@ const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
-const countCartItems = (cartItem)=>{
-  const total = cartItem.reduce((accum,curValue)=>accum+curValue.quantity,0)
-}
+const removeCartItem = (cartItems, productToRemove) => {
+  // if the quantity of the item is  equal to 1 remove the item from [cartItems] list
+  if (productToRemove.quantity === 1)
+    return cartItems.filter((cartItem) => cartItem.id !== productToRemove.id);
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === productToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
 
 export const CartDropdownContext = createContext({
   isCartOpen: null,
   setIsCartOpen: () => null,
   cartItems: [],
   addItemToCart: () => null,
-  cartCount:0
+  removeItemFromCart: () => null,
+  cartCount: 0,
 });
 
 export const CartDropdownProvider = ({ children }) => {
@@ -37,18 +46,30 @@ export const CartDropdownProvider = ({ children }) => {
   const [cartItems, setCartItem] = useState([]);
   const [cartCount, setCartCount] = useState(0);
 
-  useEffect(()=>{
-   const newCartCount = cartItems.reduce((total,cartItem)=>total+cartItem.quantity,0)
-   setCartCount(newCartCount)
-  },[cartItems]);
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
     setCartItem(addCartItem(cartItems, productToAdd));
   };
 
-  countCartItems(cartItems)
-  
-  const value = { isCartOpen, cartItems,cartCount,setIsCartOpen, addItemToCart };
+  const removeItemFromCart = (productToRemove) => {
+    setCartItem(removeCartItem(cartItems, productToRemove));
+  };
+
+  const value = {
+    isCartOpen,
+    cartItems,
+    cartCount,
+    setIsCartOpen,
+    addItemToCart,
+    removeItemFromCart,
+  };
 
   return (
     <CartDropdownContext.Provider value={value}>
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,27 +1,37 @@
 import { useContext } from "react";
 import { CartDropdownContext } from "../../contexts/cardDropdown.context";
+import CheckoutItem from "../../components/checkout-Item/checkout-Item.component";
 
 import "./checkout.styles.scss";
 
 const Checkout = () => {
-  const { cartItems,addItemToCart } = useContext(CartDropdownContext);
+  const { cartItems, addItemToCart, removeItemFromCart } =
+    useContext(CartDropdownContext);
 
   return (
-    <div>
-      <h1>I am the checkout page</h1>
-      <div>
-        {cartItems.map((cartItem) => {
-          const { id, name, quantity } = cartItem;
-          return (
-            <div key={id}>
-              <h2>{name}</h2>
-              <button>-</button> &nbsp;
-              <span>{quantity}</span> &nbsp;
-              <button onClick={() => addItemToCart(cartItem)}>+</button>
-            </div>
-          );
-        })}
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
       </div>
+
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <span className="total">Total:0</span>
     </div>
   );
 };
